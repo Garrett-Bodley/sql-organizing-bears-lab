757be820eda8d74457a77ee5c74b370a5ca369c8
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-18 15:49:11 -0400

    Archive Flatiron code.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 7, 'male', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'male', 'tan', 'rowdy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 9, 'female', 'dark brown', 'anxious', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 11, 'male', 'brown', 'serious', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 6, 'female', 'rust', 'kind', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 14, 'male', 'orange brown', 'rude', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 3, 'female', 'brown', 'chill', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 7, 'male', 'brown', 'murderous', 0);
